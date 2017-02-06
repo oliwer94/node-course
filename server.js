77e523a972dada65aa0a0ce2f580809746a2d0ec
oliwer94 <oliwer94@gmail.com>
2017-02-06 23:54:25 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,69 @@
+//starting server
+//binding to port
+//rounting
+const hbs = require('hbs');
+const express = require('express');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials')
+app.set('view engine','hbs');
+
+
+//register middleware
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    fs.appendFile('server.log',log+'\n', (err) => {
+        if(err)
+        {
+            console.log('Unable to append to server.log.')
+        }
+    });
+    console.log( log);
+    next();
+
+});
+
+app.use((req,res,next) => {
+    res.render('maintenance.hbs');
+  });
+
+app.use(express.static(__dirname+"/public"));
+
+hbs.registerHelper('getCurrentYear',() => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt',(text) =>
+{
+   return text.toUpperCase();
+});
+
+
+app.get('/',(req,res) => {
+
+    res.send({
+        name:"asdasd",
+        json:"json"
+    });
+
+});
+
+app.get('/about',(req,res) => {
+   res.render('about.hbs',{
+       pageTitle:'About Page'
+   });
+});
+
+app.get('/bad',(req,res) => {
+   res.send({
+        error_messsage:"The conn was interrupted"
+    });
+});
+
+
+app.listen(3000,()=>{
+    console.log("Server is up on port 3000!");
+});
